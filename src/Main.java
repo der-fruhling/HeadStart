ddd71446b8c0aecd0a5cb388451da992915ddb0b
Liam <liam@liamiam.com>
2018-03-19 14:11:48 -0700

    Add more classes and copyright

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018. Liam Cole
+ * This class is part of a public project, HeadStart
+ */
+
+import com.headstart.HotDog;
+import com.headstart.LinkedHotDog;
+
+public class Main {
+    public static void main(String[] args) {
+        HotDog<String, Integer> hotDog = new LinkedHotDog<String, Integer>();
+
+    }
+}
